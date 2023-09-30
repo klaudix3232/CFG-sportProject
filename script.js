50fa83cb824f3c00cb728077d37bdbfd788e1036
klaudix3232 <klaudix121314@gmail.com>
2023-09-30 16:13:46 +0100

    add a API

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 const showMessageButton = document.getElementById('showMessageButton');
 const messageElement = document.getElementById('message');
 
+const apiUrl = 'http://api.football-data.org/v4/competitions/';
+const corsProxyUrl = 'https://corsproxy.io/?' + encodeURIComponent(apiUrl);
+
 // Function to show the message when the button is clicked
 showMessageButton.addEventListener('click', function() {
   // Set the text of the message element
-  messageElement.textContent = 'Hello, this is a simple web app!';
+    fetch(corsProxyUrl, {
+        method: 'GET',
+    })
+    .then(response => {
+        return response.json()}
+        )
+    .then(data => {
+        console.log(data.competitions[0].name);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 });
