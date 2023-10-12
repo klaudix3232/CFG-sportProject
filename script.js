e653b2fb4a0820b508e080684c22866841b46668
klaudix3232 <klaudix121314@gmail.com>
2023-10-12 17:55:43 +0100

    add pages

--- script.js
+++ script.js
@@ -4,24 +4,41 @@ const messageElement = document.getElementById('message');
 
 const apiUrl = 'https://www.balldontlie.io/api/v1/players';
 
+let currentPage = 1;
+
 // Function to show the message when the button is clicked
 showMessageButton.addEventListener('click', function() {
-  // Set the text of the message element
-    fetch(apiUrl, {
-        method: 'GET',
-    })
-    .then(response => {
-        return response.json()}
-        )
-    .then(data => {
-      data.data.forEach(player => {
-        addPlayer(player);
-      });
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+  fetchPlayers(currentPage);
 });
+
+const fetchPlayers = (page) => {
+
+  apiUrlwithPage = apiUrl + '?page=' + page;
+  console.log(apiUrlwithPage);
+    // Set the text of the message element
+    fetch(apiUrlwithPage, {
+      method: 'GET',
+  })
+  .then(response => {
+      return response.json()}
+      )
+  .then(data => {
+    clearPlayerList();
+    data.data.forEach(player => {
+      addPlayer(player);
+    });
+
+    showPreviousNextButtons();
+  })
+  .catch(error => {
+      console.error('Error fetching data:', error);
+  });
+}
+
+const clearPlayerList = () => {
+  var ulElement = document.getElementById("player-list");
+  ulElement.innerHTML = '';
+}
 
 const addPlayer = (player) => {
     // Select the <ul> element using its ID
@@ -35,4 +52,26 @@ const addPlayer = (player) => {
 
     // Append the <li> element to the <ul> element
     ulElement.appendChild(liElement);
+}
+
+const showPreviousNextButtons = () => {
+  const previousButton = document.getElementById('previousButton');
+  const nextButton = document.getElementById('nextButton');
+
+  previousButton.style.display = 'block';
+  nextButton.style.display = 'block';
+};
+
+const nextPage = () => {
+  currentPage++;
+  fetchPlayers(currentPage);
+
+}
+
+const previousPage = () => {
+  if( currentPage === 1) {
+    return;
+  }
+  currentPage--;
+  fetchPlayers(currentPage);
 }
